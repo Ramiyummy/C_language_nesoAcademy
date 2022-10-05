b5fae3bbff973f86876cbfd1fa8c63c14871e3db
Ramiyummy <ramibayumi@gmail.com>
2022-10-05 11:12:31 +0100

    little practice of C codes

---
+++ mutliDimensionalSolved1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+Write a program that reads a 5*5 array of intergers and then prints the row sum and the column sum. 
+enter row1: 8   3   9   0   10
+enter row2: 3   5   17  1   1 
+enter row3: 2   8   6   23  1 
+enter row4: 15  7   3   2   9
+enter row5: 6   14  2   6   0
+
+Row T:      30  27  40  36  28
+Column T:   34  37  37  32  21
+*/
+int main(void)
+{
+  int arr[5][5] = {
+    {8, 3, 9, 0, 10},
+    {3, 5, 17, 1, 1},
+    {2, 8, 6, 23, 1},
+    {15, 7, 3, 2, 9},
+    {6, 14, 2, 6, 0}
+  };
+  return (0);
+}
