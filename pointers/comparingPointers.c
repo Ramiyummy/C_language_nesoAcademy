b5a59b7db29c295bb4d2d95683b044e78f4798d0
Ramiyummy <ramibayumi@gmail.com>
2022-10-06 19:33:15 +0100

    nesoAcademy C course

--- pointers/comparingPointers.c
+++ pointers/comparingPointers.c
@@ -14,5 +14,18 @@ int main(void)
   q = &a[3];
   
   printf("%d\n", p==q);
+
+  //homework program:
+
+  int aa[] = {5, 16, 7, 89, 45, 32, 23, 10};
+
+  int *pp = &aa[1], *qq = &aa[5];
+
+  printf("%d ", *(pp+3)); //  its points on 16, and we moved 3 step forwards, thus 45  
+  printf("%d ", *(qq-3)); //  7
+  printf("%d ", qq - pp); // the difference between the two is 4. 6 - 2 =4, or 5 -1 =4. same thing 
+  printf("%d ", pp < qq); // pp is in fact before qq, thus the output is: 1 
+  printf("%d ", *pp< *qq);// 16<32 is true, thus 1
+  // output: 45 7 4 1 1 
   return (0);
 }
