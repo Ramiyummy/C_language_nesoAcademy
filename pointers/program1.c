a0a56438900e8d019958d48751175027b8d2db9d
Ramiyummy <ramibayumi@gmail.com>
2022-10-06 19:51:00 +0100

    nesoAcademy C course

---
+++ pointers/program1.c
@@ -0,0 +1,16 @@
+/*title: sum of array elements using pointers*/
+
+#include <stdio.h>
+
+int main(void)
+{
+  int a[] = {11, 22, 36, 5, 2};
+  int sum = 0, *p;
+
+  for(p = &a[0]; p<= &a[4]; p++)
+  {
+    sum += *p;
+  }
+  printf("Sum is %d", sum);
+  return (0);
+}
