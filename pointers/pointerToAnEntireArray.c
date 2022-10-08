5abf58ac4c9f77cd2229621e57e44196de694b3d
Ramiyummy <ramibayumi@gmail.com>
2022-10-08 15:47:41 +0100

    nesoAcademy C course

--- pointers/pointerToAnEntireArray.c
+++ pointers/pointerToAnEntireArray.c
@@ -5,5 +5,8 @@ title: pointer pointing to an entire array
 
 int main(void)
 {
+  int a[5] = {1, 2, 3, 4, 5};
+  int (*p)[5] = &a; // pointer to the whole array of 5 elements, we used & bcause we want a to be the address of the whole array, and not only the first element of it :)
+  printf("%d", **p);
  return (0); 
 }
