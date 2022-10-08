61d46368ac5bceedfddfec7fb9cd3f618e56d4f8
Ramiyummy <ramibayumi@gmail.com>
2022-10-08 15:05:12 +0100

    nesoAcademy C course

---
+++ pointers/addressArithmeticOfMultidimensionalArrays.c
@@ -0,0 +1,15 @@
+/*
+title: Address Arithmetic of Multidimensional arrays.
+in 2D array: *a points to the element of the first 1D array. Thus, the addres of the first element of the 1st 1D array.
+thus: *a = *(a+0) = a[0] = &a[0][0]
+in order to get the value, we do: **a, which is 1 in this array a[2][2]={1,2,3,4};
+-------------------------------------------------------------------------------------------
+in 3D array: a[2][2][2], a is the pointer to the first 2D array,
+*/
+
+int main(void)
+{
+  
+
+  return (0);
+}
