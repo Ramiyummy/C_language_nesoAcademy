a64d24ef6a1a76869a7070093dba3365047cd52a
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-11 19:06:13 +0100

    Update solved6.c

--- strings/solved6.c
+++ strings/solved6.c
@@ -5,6 +5,6 @@ int main(void)
 {
   char *c = "GATECSIT2017";
   char *p = c;
-  printf("%d", (int)strlen(c+2[p]-6[p]-1));
+  printf("%d", (int)strlen(c+2[p]-6[p]-1)); //(1000+*(2+P)-*(6+P)-1)
   return (0);
 }
