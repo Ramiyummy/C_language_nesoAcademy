5ed2fa6f0fc107a798d33a1d9afeb681cf28365e
Ramiyummy <ramibayumi@gmail.com>
2022-10-12 18:30:10 +0100

    nesoAcademy C course

---
+++ structuresInC/structurePadding.c
@@ -0,0 +1,17 @@
+/*
+-title: Structure Padding.
+-When an object of some structure type is declared then some contiguous block of memory will be allocatged to structure members.
+*/
+#include <stdio.h>
+struct abc
+  {
+    char a;
+    char b;
+    int c;
+  } var;
+
+int main(void)
+{
+  
+  return (0);
+}
