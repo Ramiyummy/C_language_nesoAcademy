e71bf9ffef56533174dd07ed5c8954eb5cc75c33
Rami(0x0) | رامي بيومي <86990997+Ramiyummy@users.noreply.github.com>
2022-10-12 12:01:53 +0100

    Create one.c

---
+++ structuresInC/one.c
@@ -0,0 +1,16 @@
+/*
+-A Structure: is a user defined data type that can be used to group elements of different types into a single type.
+*/
+
+void doSomething(void)
+{
+  printf("No, :)");  
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+  doSomething;
+  return (0); 
+}
