d74544cef453e3dcfc454a7bbe7e9039c454ea72
Ramiyummy <ramibayumi@gmail.com>
2022-10-12 14:30:22 +0100

    nesoAcademy C course

--- functionPointers/applicationOfFunctionPointers.c
+++ functionPointers/applicationOfFunctionPointers.c
@@ -1,6 +1,7 @@
 /*
 -Title: Designing a calculator program using function pointers.
 -There are some situations in which user has to decide which function has to be called at a particular point in time.
+- We can use functin pointers to call a particular function at run time.
 */
 #include <stdio.h>
 #define ops 4
@@ -14,10 +15,13 @@ int main(void)
   float (*ptr2fun[ops])(float, float) = {sum, sub, mult, divi}; // array of pointer, which are pointers to functions.
   int choice;
   float a, b;
-  printf("Enter your choice: 0 for sum, 1 for sub, 2 for mult, 3 for div:\n");
+
+  printf("Enter your choice: 0 for sum, 1 for subtraction, 2 for multiplication, and 3 for division:\n");
   scanf("%d", &choice);
+
   printf("Enter the two number:\n");
   scanf("%f %f", &a, &b);
+
   printf("%f", ptr2fun[choice](a, b));
   return (0);
 }
