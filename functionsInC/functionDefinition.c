52068f7d18fff717ede2da33c371f1f71d3ccf7d
Ramiyummy <ramibayumi@gmail.com>
2022-10-13 10:26:05 +0100

    nesoAcademy C course

--- functionsInC/functionDefinition.c
+++ functionsInC/functionDefinition.c
@@ -1,6 +1,8 @@
 /*
 -title: Function definition.
 -Function Definition: consists of block of code which is capable of performing some specific task.
+-Parameter: is a variable in the declaration and definition of the function.
+-Argument: is the actual value of the parameter that gets passed to the fucntion.
 */
 #include <stdio.h>
 
