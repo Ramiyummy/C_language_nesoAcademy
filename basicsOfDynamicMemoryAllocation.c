4c0616d18e453508640fc455b6752b8e1f5bbadd
Ramiyummy <ramibayumi@gmail.com>
2022-10-20 09:54:14 +0100

    nesoAcademy C course

---
+++ basicsOfDynamicMemoryAllocation.c
@@ -0,0 +1,19 @@
+/*
+# Static Memory Allocation: memory allocated during compile time is called static memory. The memory allocated
+is fixed and cannot be incrased or decreased during run time.
+# Dynamic Memory Allocatin: the process of allocating memory at the time of execution is called dynamic
+memory allocation.
+#Heap: is a the segment of memory where dynamic memory allocation takes place.
+-Unlike stack where memory is allocated or deallocated in a defined order, heap is an area of memory where memory
+is allocated or opposite without any order or randomly.
+# Pointer play an important role in Dynamic Memory Allocation.
+-Allocated memory can only be accessed through pointers.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+  return (0);
+}
